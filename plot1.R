8efaaa31bb0d00a1efd57f23f0a434da86496e00
sabeer6870 <sabeer6870@gmail.com>
2014-11-13 09:32:12 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,6 @@
+plot1<-function(){
+  data<-readData()
+  png(filename = "plot1.png", width = 480, height = 480, units = "px", bg="transparent")
+  hist(data$Global_active_power,col="red",xlab="Global Active Power(kilowatts)",main="Global Active Power")
+  dev.off()
+}
