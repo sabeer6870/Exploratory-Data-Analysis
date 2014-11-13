bb89d1f8c5827d761eec9d869eb1a99aae4d9b13
sabeer6870 <sabeer6870@gmail.com>
2014-11-13 09:31:28 -0500

    Update and rename Code.R to readData.R

--- Code.R
+++
@@ -1,3 +0,0 @@
-#read data 
-data <- read.table("household_power_consumption.txt",head=TRUE,sep=";",na.strings="?")
-data<-data[(data$Date >= "2007-02-01" & data$Date<= "2007-02-02"),]
---
+++ readData.R
@@ -0,0 +1,10 @@
+readData <- function(){
+  url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  if(!file.exists(FileName)) {
+      download.file(url=url, destfile=powerdata)
+  }
+  household_power_consumption.txt<-unz(powerdata,"household_power_consumption.txt")
+  data <- read.table("household_power_consumption.txt",head=TRUE,sep=";",na.strings="?")
+  data<-data[(data$Date >= "2007-02-01" & data$Date<= "2007-02-02"),]
+  data
+}
