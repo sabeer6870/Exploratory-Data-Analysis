1cf9f9fef5ae8f3d854464d6106e8fabb7c22869
sabeer6870 <sabeer6870@gmail.com>
2014-11-14 23:23:10 -0500

    Update readData.R

--- readData.R
+++ readData.R
@@ -1,10 +1,12 @@
 readData <- function(){
   url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-  if(!file.exists(FileName)) {
+  powerdata<-"powerconsumption.zip"
+  if(!file.exists(powerdata)) {
       download.file(url=url, destfile=powerdata)
   }
   household_power_consumption.txt<-unz(powerdata,"household_power_consumption.txt")
   data <- read.table("household_power_consumption.txt",head=TRUE,sep=";",na.strings="?")
+  data$Date<-strptime(as.character(data$Date), "%d/%m/%Y")
   data<-data[(data$Date >= "2007-02-01" & data$Date<= "2007-02-02"),]
   data$newDate<-as.POSIXct(paste(data$Date, data$Time))
   data
